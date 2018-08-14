28bfa33b3643b2c9e1f5d51ef673d8e466bdf732
1021237228 <1021237228@qq.com>
2018-08-14 23:20:48 +0800

    自定义控件

---
+++ app/src/main/java/com/gll/gllandroidstudy/view/CustomTitleView.java
@@ -0,0 +1,151 @@
+package com.gll.gllandroidstudy.view;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.View;
+
+import com.gll.gllandroidstudy.R;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+/**
+ * Created by: Z_B on 2018/8/14.
+ * Function:
+ */
+public class CustomTitleView extends View {
+    private String mTitleText;
+    /**
+     * 文本的颜色
+     */
+    private int mTitleTextColor;
+    /**
+     * 文本的大小
+     */
+    private int mTitleTextSize;
+
+    /**
+     * 绘制时控制文本绘制的范围
+     */
+    private Rect mBound;
+    private Paint mPaint;
+
+
+    public CustomTitleView(Context context) {
+        this(context, null);
+    }
+
+    public CustomTitleView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CustomTitleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        TypedArray typedArray = context.getTheme().obtainStyledAttributes(attrs, R.styleable.CustomTitleView, defStyleAttr, 0);
+        int n = typedArray.getIndexCount();
+        for (int i = 0; i < n; i++) {
+            int attr = typedArray.getIndex(i);
+            switch (attr) {
+                case R.styleable.CustomTitleView_cusTextViewText:
+                    mTitleText = typedArray.getString(attr);
+                    break;
+                case R.styleable.CustomTitleView_cusTextViewColor:
+                    // 默认颜色设置为黑色
+                    mTitleTextColor = typedArray.getColor(attr, Color.BLACK);
+                    break;
+                case R.styleable.CustomTitleView_cusTextViewSize:
+                    // 默认设置为16sp，TypeValue也可以把sp转化为px
+                    mTitleTextSize = typedArray.getDimensionPixelSize(attr, (int) TypedValue.applyDimension(
+                            TypedValue.COMPLEX_UNIT_SP, 16, getResources().getDisplayMetrics()));
+                    break;
+
+            }
+
+        }
+        typedArray.recycle();
+        /**
+         * 获得绘制文本的宽和高
+         */
+        mPaint = new Paint();
+        mPaint.setTextSize(mTitleTextSize);
+        // mPaint.setColor(mTitleTextColor);
+        mBound = new Rect();
+        mPaint.getTextBounds(mTitleText, 0, mTitleText.length(), mBound);
+
+        this.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTitleText = randomText();
+                postInvalidate();
+            }
+        });
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        int width;
+        int height;
+        if (widthMode == MeasureSpec.EXACTLY) {
+            width = widthSize;
+        } else {
+            mPaint.setTextSize(mTitleTextSize);
+            mPaint.getTextBounds(mTitleText, 0, mTitleText.length(), mBound);
+            float textWidth = mBound.width();
+            int desired = (int) (getPaddingLeft() + textWidth + getPaddingRight());
+            width = desired;
+        }
+
+        if (heightMode == MeasureSpec.EXACTLY) {
+            height = heightSize;
+        } else {
+            mPaint.setTextSize(mTitleTextSize);
+            mPaint.getTextBounds(mTitleText, 0, mTitleText.length(), mBound);
+            float textHeight = mBound.height();
+            int desired = (int) (getPaddingTop() + textHeight + getPaddingBottom());
+            height = desired;
+        }
+        setMeasuredDimension(width, height);
+
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        mPaint.setColor(Color.YELLOW);
+        canvas.drawRect(0, 0, getMeasuredWidth(), getMeasuredHeight(), mPaint);
+
+        mPaint.setColor(mTitleTextColor);
+        canvas.drawText(mTitleText, getWidth() / 2 - mBound.width() / 2, getHeight() / 2 + mBound.height() / 2, mPaint);
+
+    }
+
+
+    private String randomText() {
+        Random random = new Random();
+        Set<Integer> set = new HashSet<Integer>();
+        while (set.size() < 4) {
+            int randomInt = random.nextInt(10);
+            set.add(randomInt);
+        }
+        StringBuffer sb = new StringBuffer();
+        for (Integer i : set) {
+            sb.append("" + i);
+        }
+        return sb.toString();
+    }
+
+}
