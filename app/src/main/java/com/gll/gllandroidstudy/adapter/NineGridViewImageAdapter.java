56ade41e6f9e16ee47c75f077afcbbb347a7a9c9
Administrator <1021237228@qq.com>
2019-04-03 10:59:01 +0800

    九宫格图片的处理

--- app/src/main/java/com/gll/gllandroidstudy/adapter/NineGridViewImageAdapter.java
+++ app/src/main/java/com/gll/gllandroidstudy/adapter/NineGridViewImageAdapter.java
@@ -2,24 +2,18 @@ package com.gll.gllandroidstudy.adapter;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.chad.library.adapter.base.BaseQuickAdapter;
-import com.chad.library.adapter.base.BaseViewHolder;
 import com.gll.gllandroidstudy.R;
 import com.gll.gllandroidstudy.model.NineImageList;
-import com.gll.gllandroidstudy.nineimage.GridImageView;
 import com.gll.gllandroidstudy.nineimage.NineGridImageView;
 import com.gll.gllandroidstudy.nineimage.NineGridImageViewAdapter;
 
-import java.security.PrivateKey;
 import java.util.List;
 
 /**
