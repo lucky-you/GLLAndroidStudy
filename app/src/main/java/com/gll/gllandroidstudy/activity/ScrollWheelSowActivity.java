929402d4ac72f7812168e6b62fdecba811718bea
Administrator <1021237228@qq.com>
2019-04-28 18:01:04 +0800

    自定义banner的实现

--- app/src/main/java/com/gll/gllandroidstudy/activity/ScrollWheelSowActivity.java
+++ app/src/main/java/com/gll/gllandroidstudy/activity/ScrollWheelSowActivity.java
@@ -1,12 +1,17 @@
 package com.gll.gllandroidstudy.activity;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.widget.TextView;
 
 import com.gll.gllandroidstudy.R;
 import com.gll.gllandroidstudy.base.BaseActivity;
+import com.gll.gllandroidstudy.view.BannerLayout;
 import com.gll.gllandroidstudy.view.CustomCountDownTimerView;
 import com.gll.gllandroidstudy.view.RunTextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 滚动轮播的activity
@@ -16,6 +21,7 @@ public class ScrollWheelSowActivity extends BaseActivity {
     private TextView tvTitleRadio;
     private RunTextView tvRunTextView;
     private CustomCountDownTimerView customCountDownTimeView;
+    private BannerLayout bannerView;
 
     @Override
     protected void loadViewLayout() {
@@ -28,6 +34,7 @@ public class ScrollWheelSowActivity extends BaseActivity {
         tvTitleRadio = get(R.id.tvTitleRadio);
         tvRunTextView = get(R.id.tvRunTextView);
         customCountDownTimeView = get(R.id.customCountDownTimeView);
+        bannerView = (BannerLayout) findViewById(R.id.bannerView);
     }
 
     @Override
@@ -37,6 +44,22 @@ public class ScrollWheelSowActivity extends BaseActivity {
         tvRunTextView.startAnim();
         customCountDownTimeView.setCountdownTime(10);
         customCountDownTimeView.startCountDown();
+
+
+        List<Object> bannerList = new ArrayList<>();
+        bannerList.add("http://img.mukewang.com/55237dcc0001128c06000338.jpg");
+        bannerList.add("http://img.mukewang.com/551e470500018dd806000338.jpg");
+        bannerList.add("http://img.mukewang.com/5518c3d7000175af06000338.jpg");
+        bannerList.add("http://img.mukewang.com/551916790001125706000338.jpg");
+
+        bannerView.setBannerPointSize(10);
+        bannerView.setBannerPointGravity(Gravity.CENTER);
+        bannerView.setBannerPointDrawableSelected(R.drawable.wall11);
+        bannerView.setBannerPointDrawableUnselected(R.drawable.wall12);
+        bannerView.setBannerDelaySecond(5);
+        //banner 设置方法完毕时最后调用 start 方法
+        bannerView.start(bannerList);
+
     }
 
     @Override
@@ -47,5 +70,18 @@ public class ScrollWheelSowActivity extends BaseActivity {
                 showToast("倒计时完成");
             }
         });
+
+        bannerView.setOnBannerClickListener(new BannerLayout.OnBannerClickListener() {
+            @Override
+            public void onBannerClick(int position) {
+                showToast("点击了" + position);
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        bannerView.bannerShutdown();
     }
 }
