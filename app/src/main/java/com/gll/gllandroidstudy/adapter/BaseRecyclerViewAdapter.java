435fcb7ccb90a77a136095573862902d052cd129
Administrator <1021237228@qq.com>
2019-05-23 19:12:22 +0800

    baseRecyclerView的完善

--- app/src/main/java/com/gll/gllandroidstudy/adapter/BaseRecyclerViewAdapter.java
+++ app/src/main/java/com/gll/gllandroidstudy/adapter/BaseRecyclerViewAdapter.java
@@ -1,11 +1,15 @@
 package com.gll.gllandroidstudy.adapter;
 
 import android.support.annotation.LayoutRes;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.gll.gllandroidstudy.R;
 import com.gll.gllandroidstudy.base.rv.BaseViewHolder;
 import com.gll.gllandroidstudy.base.rv.adapter.SimpleAdapter;
 import com.gll.gllandroidstudy.model.RecyclerViewList;
+import com.gll.gllandroidstudy.widget.GlideRoundTransform;
 
 import java.util.List;
 
@@ -20,6 +24,14 @@ public class BaseRecyclerViewAdapter extends SimpleAdapter<RecyclerViewList> {
 
     @Override
     protected void bind(BaseViewHolder holder, RecyclerViewList data) {
+        String imageUrl = "http://img.mukewang.com/547d5a45000156f406000338.jpg";
+        RequestOptions myOptions = new RequestOptions()
+                .centerCrop()
+                .transform(new GlideRoundTransform(mContext, 6));
+        Glide.with(mContext)
+                .load(imageUrl)
+                .apply(myOptions)
+                .into((ImageView) holder.getView(R.id.ivMessageUrl));
         holder.setText(R.id.tvMessageTitle, data.getTitle())
                 .setText(R.id.tvMessageContent, data.getContent());
     }
